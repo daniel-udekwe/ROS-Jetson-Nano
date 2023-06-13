4f053ad4713962506594a16c618909d085190b97
daniel-udekwe <nickudekwe@gmail.com>
2023-06-13 12:36:32 -0400

    after installation

---
+++ build/zed-ros-wrapper/zed-ros-interfaces/CMakeFiles/zed_interfaces_generate_messages_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,35 @@
+file(REMOVE_RECURSE
+  "/home/jetson/catkin_ws/devel/include/zed_interfaces/BoundingBox2Df.h"
+  "/home/jetson/catkin_ws/devel/include/zed_interfaces/BoundingBox2Di.h"
+  "/home/jetson/catkin_ws/devel/include/zed_interfaces/BoundingBox3D.h"
+  "/home/jetson/catkin_ws/devel/include/zed_interfaces/Keypoint2Df.h"
+  "/home/jetson/catkin_ws/devel/include/zed_interfaces/Keypoint2Di.h"
+  "/home/jetson/catkin_ws/devel/include/zed_interfaces/Keypoint3D.h"
+  "/home/jetson/catkin_ws/devel/include/zed_interfaces/Object.h"
+  "/home/jetson/catkin_ws/devel/include/zed_interfaces/ObjectsStamped.h"
+  "/home/jetson/catkin_ws/devel/include/zed_interfaces/PlaneStamped.h"
+  "/home/jetson/catkin_ws/devel/include/zed_interfaces/RGBDSensors.h"
+  "/home/jetson/catkin_ws/devel/include/zed_interfaces/Skeleton2D.h"
+  "/home/jetson/catkin_ws/devel/include/zed_interfaces/Skeleton3D.h"
+  "/home/jetson/catkin_ws/devel/include/zed_interfaces/reset_odometry.h"
+  "/home/jetson/catkin_ws/devel/include/zed_interfaces/reset_tracking.h"
+  "/home/jetson/catkin_ws/devel/include/zed_interfaces/save_3d_map.h"
+  "/home/jetson/catkin_ws/devel/include/zed_interfaces/save_area_memory.h"
+  "/home/jetson/catkin_ws/devel/include/zed_interfaces/set_led_status.h"
+  "/home/jetson/catkin_ws/devel/include/zed_interfaces/set_pose.h"
+  "/home/jetson/catkin_ws/devel/include/zed_interfaces/start_3d_mapping.h"
+  "/home/jetson/catkin_ws/devel/include/zed_interfaces/start_object_detection.h"
+  "/home/jetson/catkin_ws/devel/include/zed_interfaces/start_remote_stream.h"
+  "/home/jetson/catkin_ws/devel/include/zed_interfaces/start_svo_recording.h"
+  "/home/jetson/catkin_ws/devel/include/zed_interfaces/stop_3d_mapping.h"
+  "/home/jetson/catkin_ws/devel/include/zed_interfaces/stop_object_detection.h"
+  "/home/jetson/catkin_ws/devel/include/zed_interfaces/stop_remote_stream.h"
+  "/home/jetson/catkin_ws/devel/include/zed_interfaces/stop_svo_recording.h"
+  "/home/jetson/catkin_ws/devel/include/zed_interfaces/toggle_led.h"
+  "CMakeFiles/zed_interfaces_generate_messages_cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/zed_interfaces_generate_messages_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ devel/share/gennodejs/ros/zed_interfaces/msg/_index.js
@@ -0,0 +1,30 @@
+
+"use strict";
+
+let Object = require('./Object.js');
+let BoundingBox2Di = require('./BoundingBox2Di.js');
+let PlaneStamped = require('./PlaneStamped.js');
+let ObjectsStamped = require('./ObjectsStamped.js');
+let Skeleton3D = require('./Skeleton3D.js');
+let Keypoint3D = require('./Keypoint3D.js');
+let Keypoint2Df = require('./Keypoint2Df.js');
+let RGBDSensors = require('./RGBDSensors.js');
+let BoundingBox2Df = require('./BoundingBox2Df.js');
+let Keypoint2Di = require('./Keypoint2Di.js');
+let Skeleton2D = require('./Skeleton2D.js');
+let BoundingBox3D = require('./BoundingBox3D.js');
+
+module.exports = {
+  Object: Object,
+  BoundingBox2Di: BoundingBox2Di,
+  PlaneStamped: PlaneStamped,
+  ObjectsStamped: ObjectsStamped,
+  Skeleton3D: Skeleton3D,
+  Keypoint3D: Keypoint3D,
+  Keypoint2Df: Keypoint2Df,
+  RGBDSensors: RGBDSensors,
+  BoundingBox2Df: BoundingBox2Df,
+  Keypoint2Di: Keypoint2Di,
+  Skeleton2D: Skeleton2D,
+  BoundingBox3D: BoundingBox3D,
+};
